cba0fbf43bfdadba01ee3bfe1665e959f6876e5b
Julien Troussier <troussier.julien@gmail.com>
2015-12-14 20:26:36 +0100

    update MakeFiles.txt

---
+++ Snakz/build/CMakeFiles/Snakz.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snakz.dir/src/main.cpp.obj"
+  "Snakz.pdb"
+  "Snakz.exe"
+  "Snakz.exe.manifest"
+  "libSnakz.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snakz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
